733f2793a915541fa9f4dd215a1798c0271b3d3e
allairemorgan <allairemorgan99@gmial.com>
2019-05-29 14:48:57 -0400

    Parsing changes for heart data

--- Parsing.java
+++ Parsing.java
@@ -8,32 +8,19 @@ public class Parsing {
 	int q2;
 	int q3;
 	int q4;
-	int q5;
-	int q6;
-	int q7;
-	int q8;
-	int q9;
-	int q10;
 	String answer;
 	
-	public Parsing (int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
+	public Parsing (int a, int b, int c, int d) {
 		q1 = a;
 		q2 = b;
 		q3 = c;
 		q4 = d;
-		q5 = e;
-		q6 = f;
-		q7 = g;
-		q8 = h;
-		q9 = i;
-		q10 = j;
 		answer = "";
 	}
 	
 	//Append everything to the answer, write the data to a file preceded by a ;
 	public void submit(String filename) {
-		answer = answer + q1 + "," + q2 + "," + q3 + "," + q4 + "," + q5 +
-                  "," + q6 + "," + q7 + "," + q8 + "," + q9 + "," + q10 + ";";
+		answer = answer + q1 + "," + q2 + "," + q3 + "," + q4 + ";";
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(filename));
 			out.write(answer);
